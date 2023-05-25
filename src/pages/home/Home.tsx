b96fe950c571307c3c7b7c3430abc67d6111a13a
Serhii Veretennikov <gridis72@gmail.com>
2023-05-25 23:22:52 +0100

    added product sections and mobile markup for them

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,24 +1,19 @@
 import { FC } from "react";
-import "./Home.css"
+import "./Home.css";
 import { Link } from "react-router-dom";
 import MainNav from "../../components/mainNav/MainNav";
 
-const Home: FC = () => {  
+const Home: FC = () => {
   return (
     <main>
       <section className="headliner">
-        <p className="headliner__overline">
-          new product
-        </p>
-        <h1>
-          XX99 Mark II Headphones
-        </h1>
+        <p className="headliner__overline">new product</p>
+        <h1>XX99 Mark II Headphones</h1>
         <p className="headliner__desc">
-          Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
         </p>
-        <Link to={"/headphones/xx99-markii"}>
-          See product
-        </Link>
+        <Link to={"/headphones/xx99-markii"}>See product</Link>
       </section>
       <section className="mainNavSection">
         <MainNav />
@@ -27,14 +22,66 @@ const Home: FC = () => {
         <div className="container">
           <div className="primaryProduct__imageWrapper">
             <picture>
-              <source media="(min-width: 425px)" srcSet="/assets/home/tablet/image-speaker-zx9.png" />
-              <source media="(min-width: 768px)" srcSet="/assets/home/desktop/image-speaker-zx9.png" />
-              <img src="/assets/home/mobile/image-speaker-zx9.png" alt="speaker zx9 image" />
+              <source
+                media="(min-width: 768px)"
+                srcSet="/assets/home/desktop/image-speaker-zx9.png"
+              />
+              <source
+                media="(min-width: 425px)"
+                srcSet="/assets/home/tablet/image-speaker-zx9.png"
+              />
+              <img
+                src="/assets/home/mobile/image-speaker-zx9.png"
+                alt="speaker zx9 image"
+              />
             </picture>
           </div>
-          <h1>
-            zx9 speaker
-          </h1>
+          <h1>zx9<br/>speaker</h1>
+          <p>
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <Link to={"/speakers/zx9"} >
+            See product
+          </Link>
+        </div>
+      </section>
+      <section className="secondaryProduct">
+        <div className="container">
+          <h4>
+            zx7 speaker
+          </h4>
+          <Link to={"/speakers/zx7"}>
+            See product
+          </Link>
+        </div>
+      </section>
+      <section className="tertiaryProduct">
+        <div className="container">
+          <div className="tretiaryProduct__imageWrapper">
+            <picture>
+              <source
+                media="(min-width: 768px)"
+                srcSet="/assets/home/desktop/image-earphones-yx1.jpg"
+              />
+              <source
+                media="(min-width: 425px)"
+                srcSet="/assets/home/tablet/image-earphones-yx1.jpg"
+              />
+              <img
+                src="/assets/home/mobile/image-earphones-yx1.jpg"
+                alt="speaker zx9 image"
+              />
+            </picture>
+          </div>
+          <div className="tretiaryProduct__linkWrapper">
+            <h4>
+              yx1 earphones
+            </h4>
+            <Link to={"/earphones/yx1"}>
+              See product
+            </Link>
+          </div>
         </div>
       </section>
     </main>
