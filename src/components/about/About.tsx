b0b4217fa76b31babf2614a796a696bdc6882090
Serhii Veretennikov <gridis72@gmail.com>
2023-06-06 23:40:04 +0100

    finished desktop layout(no hovers and actives yet), tweaked eslint config slightly

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,34 +2,42 @@ import { FC } from "react";
 import "./About.css";
 
 const About: FC = () => {
-  return (
-    <section className="about">
-      <div className="container">
-        <div className="about__imageWrapper">
-          <picture>
-            <source
-              media="(min-width: 769px)"
-              srcSet="/assets/shared/desktop/image-best-gear.jpg"
-            />
-            <source
-              media="(min-width: 426px)"
-              srcSet="/assets/shared/tablet/image-best-gear.jpg"
-            />
-            <img src="/assets/shared/mobile/image-best-gear.jpg" alt="" />
-          </picture>
-        </div>
-      </div>
-      <h2>Bringing you the <span>best</span> audio gear</h2>
-      <p>
-        Located at the heart of New York City, Audiophile is the premier store
-        for high end headphones, earphones, speakers, and audio accessories. We
-        have a large showroom and luxury demonstration rooms available for you
-        to browse and experience a wide range of our products. Stop by our store
-        to meet some of the fantastic people who make Audiophile the best place
-        to buy your portable audio equipment.
-      </p>
-    </section>
-  );
+    return (
+        <section className="about">
+            <div className="container">
+                <div className="about__imageWrapper">
+                    <picture>
+                        <source
+                            media="(min-width: 880px)"
+                            srcSet="/assets/shared/desktop/image-best-gear.jpg"
+                        />
+                        <source
+                            media="(min-width: 426px)"
+                            srcSet="/assets/shared/tablet/image-best-gear.jpg"
+                        />
+                        <img
+                            src="/assets/shared/mobile/image-best-gear.jpg"
+                            alt=""
+                        />
+                    </picture>
+                </div>
+                <article>
+                    <h2>
+                        Bringing you the <span>best</span> audio gear
+                    </h2>
+                    <p>
+                        Located at the heart of New York City, Audiophile is the
+                        premier store for high end headphones, earphones, speakers,
+                        and audio accessories. We have a large showroom and luxury
+                        demonstration rooms available for you to browse and
+                        experience a wide range of our products. Stop by our store
+                        to meet some of the fantastic people who make Audiophile the
+                        best place to buy your portable audio equipment.
+                    </p>
+                </article>
+            </div>
+        </section>
+    );
 };
 
 export default About;
