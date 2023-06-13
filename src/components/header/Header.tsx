51a14fc31b72f813491181361f90627998e1bf75
Serhii Veretennikov <gridis72@gmail.com>
2023-06-13 22:54:59 +0300

    added minified header to modal menu, for it to close properly when header is clicked

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC } from "react";
+import { FC } from "react";
 import logo from "/assets/shared/desktop/logo.svg";
 import cart from "/assets/shared/desktop/icon-cart.svg";
 import burger from "/assets/shared/tablet/icon-hamburger.svg";
@@ -9,25 +9,23 @@ import "./Header.css";
 const Header: FC = () => {
     // const {category, product} = useParams();
 
-    const burgerHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    const burgerHandler = (): void => {
         const modal = document.getElementById(
             "header__mainNavWrapper"
         ) as HTMLDialogElement;
 
-        if (e.target.value === "on") {
-            const body = document.body as HTMLElement;
-            body.style.overflow = "hidden";
+        const body = document.body as HTMLElement;
+        body.style.overflow = "hidden";
 
-            modal.showModal();
-            modal.addEventListener(
-                "click",
-                () => {
-                    body.style.overflow = "scroll";        
-                    modal.close();
-                },
-                { once: true }
-            );
-        }
+        modal.showModal();
+        modal.addEventListener(
+            "click",
+            () => {
+                body.style.overflow = "scroll";
+                modal.close();
+            },
+            { once: true }
+        );
     };
 
     const cartHandler = (): void => {
@@ -47,6 +45,19 @@ const Header: FC = () => {
                     <img src={burger} alt="" />
                 </div>
                 <dialog id="header__mainNavWrapper">
+                    <header className="header-dialog">
+                        <div className="container-dialog">
+                            <div className="header__checkboxCoverWrapper-dialog">
+                                <img src={burger} alt="" />
+                            </div>
+                            <Link to={"/"} className="header__logo">
+                                <img src={logo} alt="audiophile loge" />
+                            </Link>
+                            <div className="header__cart" onClick={cartHandler}>
+                                <img src={cart} alt="" />
+                            </div>
+                        </div>
+                    </header>
                     <MainNav />
                 </dialog>
                 <Link to={"/"} className="header__logo">
