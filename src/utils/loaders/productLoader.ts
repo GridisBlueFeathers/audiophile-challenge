3f3f45a985cc946ddde5f3ba1d6c3d79dc41ed15
Serhii Veretennikov <gridis72@gmail.com>
2023-09-06 01:48:59 +0200

    fix(product page): fixed path to firestore in product page loader

--- src/utils/loaders/productLoader.ts
+++ src/utils/loaders/productLoader.ts
@@ -1,5 +1,5 @@
 import { doc, getDoc } from "firebase/firestore";
-import { firestore } from "./firebase";
+import { firestore } from "../firebase/firebase";
 import { LoaderFunction } from "react-router-dom";
 
 export type ProductData = {
